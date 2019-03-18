a0adf1fdbf1e5e173e36fe6e75e0c8c499f6145c
Anatoly Popov <popov.anatoly@gmail.com>
2019-03-18 19:03:51 +0300

    django main page template ready

--- samotlor_site/urls.py
+++ samotlor_site/urls.py
@@ -13,9 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+import mainapp.views as mainapp
+# from .routers import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainapp.index, name='index'),
+    path('about/', mainapp.about, name='about'),
+    path('contact/', mainapp.contact, name='contact'),
+    path('staff/', mainapp.staff, name='staff'),
+    path('documents/', mainapp.documents, name='documents'),
+    path('news/', mainapp.news, name='news'),
+    path('detailview/<slug:content>/<slug:pk>',
+         mainapp.details, name='detailview'),
+    path('create/<slug:content_type>', mainapp.create_factory, name='create'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('messages/', mainapp.messages, name='messages'),
+    path('services/', mainapp.services, name="services"),
+    path('reestrsp/', mainapp.reestrsp, name='reestrsp'),
+    # path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
